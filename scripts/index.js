757c3ec523b7d9b2733d7bfc420f2e6827c056b5
Veniamin Ryadnov <veniamin.ryadnov@gmail.com>
2024-04-22 11:14:13 +0300

    udpate

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,34 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const content = document.querySelector('.content');
-const placesList = document.querySelector('.places__list');
+const content = document.querySelector(".content");
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 //дописать функцию для кнопки
 function createCard(name, link, deleteCard) {
-  const card = cardTemplate.querySelector('.card').cloneNode(true);
-  const deleteButton = card.querySelector('.card__delete-button');
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const deleteButton = card.querySelector(".card__delete-button");
   //заполняем данные карточки
-  card.querySelector('.card__image').src = link;
-  card.querySelector('.card__title').textContent = name;
-  
+  cardImage.src = link;
+  cardImage.alt = name;
+  card.querySelector(".card__title").textContent = name;
+
   //обработчик клика для удаления карточки колбэком
-  deleteButton.addEventListener('click', function() {
-  deleteCard(card)
+  deleteButton.addEventListener("click", function () {
+    deleteCard(card);
   });
   return card;
-};
+}
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
-    placesList.append(createCard(item.name, item.link, deleteCard));
-  });
-  
+  placesList.append(createCard(item.name, item.link, deleteCard));
+});
+
 // @todo: Функция удаления карточки
 function deleteCard(item) {
-    item.remove();
-};
-
-
-
-
+  item.remove();
+}
